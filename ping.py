9d84464366e17d8cd557d07e9da11a2786aecf5c
dsobit <sobolevdmitri85@gmail.com>
2022-05-26 12:08:13 +0300

    Eksam

--- ping.py
+++ ping.py
@@ -1,8 +1,13 @@
 import requests
 
-def check_server_status():
-    a = requests.get(url)
-    if a = 200:
-        return True
-    else:
+
+def check_server_status(ip_or_path: str):
+    """Method for checking if server is up and running."""
+    try:
+        a = requests.get(ip_or_path)
+        if a.status_code == 200:
+            return True
+        else:
+            return False
+    except Exception:
         return False
